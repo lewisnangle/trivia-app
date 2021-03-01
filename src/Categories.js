633f70484231628c43b044ddc2f4fb188eb18c89
Lewis Nangle <lewisnangle@lewiss-mbp.home>
2021-03-01 09:50:22 +0000

    Added method to initialise scores for each catagory to 0

--- src/Categories.js
+++ src/Categories.js
@@ -5,31 +5,50 @@ import {Link} from 'react-router-dom';
 
 function Catagory (){
 
-    const [catagories,setCatagories] = useState([]);
+    const [catagories,setCatagories] = useState([
+      {id : '',
+      title : '',
+      clues_count : 0,
+      score : 0}
+    ]);
 
     useEffect(() => {
       fetchCatagories();
     },[])
-  
+
+    const initialiseScores = (json) => {
+      const array = [];
+      json.map((catagory,index) => (
+        array[index] = {...catagory,score : 0}  
+      ))
+      return array;
+    }
   
     const fetchCatagories = async () => {
       const data = await fetch(`http://jservice.io/api/categories?count=30`);
       const json = await data.json();
-      setCatagories(json);
-      console.log(json)
+      setCatagories(initialiseScores(json));
+    }
+
+
+    const incrementCatagoryScore = () => {
+
+
+
     }
 
 
     return(
+      
         <div>
         {
             catagories.map(catagory => (
               <h2 key = {catagory.id}>
-              <Link to = {`/categories/${catagory.id}`}>{catagory.title}</Link>  
+              <Link to = {`/categories/${catagory.id}`}>{catagory.title}</Link>  {catagory.score}
               </h2>
               ))
           }
-
+        {console.log(catagories)}
         </div>
     )
 
